ab83f1fd0353e76c26c0d4493b8f86824db3efc5
1113724 <lcaballero017@ikasle.ehu.eus>
2026-02-24 10:03:38 +0100

    WGAN 2 canales

--- train_WGAN.py
+++ train_WGAN.py
@@ -3,7 +3,6 @@ import os
 import numpy as np
 import torch
 from torch import nn
-from torch.nn.utils import spectral_norm
 from torch.utils.data import TensorDataset, DataLoader
 
 # ----- GENERATOR Conv1D ----- #
@@ -21,7 +20,7 @@ class Generator(nn.Module):
             nn.ConvTranspose1d(64, 32, kernel_size=4, stride=2, padding=1),
             nn.ReLU(True),
 
-            nn.ConvTranspose1d(32, 1, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose1d(32, 2, kernel_size=4, stride=2, padding=1),
             nn.Tanh()
         )
 
@@ -36,16 +35,16 @@ class Discriminator(nn.Module):
     def __init__(self, L):
         super().__init__()
         self.net = nn.Sequential(
-            spectral_norm(nn.Conv1d(1, 32, 4, 2, 1)),
+            nn.Conv1d(2, 32, 4, 2, 1),
             nn.LeakyReLU(0.2),
 
-            spectral_norm(nn.Conv1d(32, 64, 4, 2, 1)),
+            nn.Conv1d(32, 64, 4, 2, 1),
             nn.LeakyReLU(0.2),
 
-            spectral_norm(nn.Conv1d(64, 128, 4, 2, 1)),
+            nn.Conv1d(64, 128, 4, 2, 1),
             nn.LeakyReLU(0.2),
         )
-        self.fc = spectral_norm(nn.Linear(128 * (L // 8), 1))
+        self.fc = nn.Linear(128 * (L // 8), 1)
 
     def forward(self, x):
         f = self.net(x)
@@ -59,7 +58,7 @@ def weights_init(m):
         if m.bias is not None:
             nn.init.zeros_(m.bias)
 
-def gradient_penalty(D, real, fake, device):
+def gradient_penalty(critic, real, fake, device):
     bsize = real.size(0)
     epsilon = torch.rand(bsize, 1, 1, device=device)
     epsilon = epsilon.expand_as(real)
@@ -67,7 +66,7 @@ def gradient_penalty(D, real, fake, device):
     interpolated = epsilon * real + (1 - epsilon) * fake
     interpolated.requires_grad_(True)
 
-    d_interpolated = D(interpolated)
+    d_interpolated = critic(interpolated)
 
     gradients = torch.autograd.grad(
         outputs=d_interpolated,
@@ -84,121 +83,109 @@ def gradient_penalty(D, real, fake, device):
 def compute_pdp(x):
     return torch.mean(x ** 2, dim=0)
 
-def train_gan(G, D, loader, device, epochs=100, lr=2e-4,
-              save_dir='checkpoints/GAN_Conv1D',patience=10,
-              min_delta=1e-4, z_dim=32):
+def train_gan(G, C, loader, device, epochs=100, lr=1e-4,
+              λ_gp=10, n_critic=5,z_dim=128,
+              patience=15, min_delta=1e-4,
+              save_dir='checkpoints/WGAN_Conv1D'):
+
     os.makedirs(save_dir, exist_ok=True)
-    # pérdida LSGAN
-    #criterion = nn.MSELoss()
     # optimizadores Adam para G y D
-    optD = torch.optim.Adam(D.parameters(), lr=lr, betas=(0.5, 0.999))
-    optG = torch.optim.Adam(G.parameters(), lr=lr, betas=(0.5, 0.999))
+    optC = torch.optim.Adam(C.parameters(), lr=lr, betas=(0.0, 0.9))
+    optG = torch.optim.Adam(G.parameters(), lr=lr, betas=(0.0, 0.9))
 
-    ### Bucle del ENTRENAMIENTO principal ###
-    # en cada iter:
-    #   - entrenar primero discriminador (D)
-    #   - luego el generador (G)
-    #   - guardar las pérdidas promedio por época
-
-    λ_gp = 10
-    λ_pdp = 0.1
-    n_critic = 5
-
-    best_g_loss = float('inf')
+    best_w_dist = -float("inf")
     epochs_no_improve = 0
 
+    ### Bucle del ENTRENAMIENTO principal ###
     for epoch in range(1, epochs + 1):
-        g_loss_avg = 0.0
-        d_loss_avg = 0.0
+        c_loss_epoch = 0.0
+        g_loss_epoch = 0.0
+        w_dist_epoch = 0.0
 
         for (real_batch,) in loader:
             real = real_batch.to(device)
-            real = real.unsqueeze(1)
             bsize = real.size(0)
 
-            d_loss_batch = 0.0
-
-            ### Entrenar Discriminador ###
+            ### Entrenar Critic ###
             for _ in range(n_critic):
-                optD.zero_grad()
                 z = torch.randn(bsize, z_dim, device=device)
                 fake = G(z).detach()
 
-                loss_D = -(D(real).mean() - D(fake).mean())
-                gp = gradient_penalty(D, real, fake, device)
+                real_score = C(real).mean()
+                fake_score = C(fake).mean()
+                loss_C = -(real_score - fake_score)
+                gp = gradient_penalty(C, real, fake, device)
 
-                loss_D_total = loss_D + λ_gp * gp
-                loss_D_total.backward()
-                optD.step()
+                loss_total = loss_C + λ_gp * gp
 
-                d_loss_batch += loss_D.item()
+                optC.zero_grad()
+                loss_total.backward()
+                optC.step()
+
+            # después de entrenar el critic, usamos los mismo batch de real/fake para W_dist
+            with torch.no_grad():
+                z = torch.randn(bsize, z_dim, device=device)
+                fake_for_wdist = G(z)
+                w_dist_epoch += (C(real).mean() - C(fake_for_wdist).mean()).item()
 
             ### Entrenar Generador ###
-            optG.zero_grad()
-
             z = torch.randn(bsize, z_dim, device=device)
             fake = G(z)
+            loss_G = -C(fake).mean()
 
-            loss_adv = -D(fake).mean()
-
-            pdp_real = compute_pdp(real).detach()
-            pdp_fake = compute_pdp(fake)
-            loss_pdp = torch.mean(torch.abs(pdp_real - pdp_fake))
-
-            loss_G_total = loss_adv + λ_pdp * loss_pdp
-            loss_G_total.backward()
+            optG.zero_grad()
+            loss_G.backward()
             optG.step()
 
-            # acumular pérdidas
-            g_loss_avg += loss_G_total.item()
-            d_loss_avg += d_loss_batch / n_critic
+            c_loss_epoch += loss_C.item()
+            g_loss_epoch += loss_G.item()
 
-        # promedios por época
-        g_loss_avg /= len(loader)
-        d_loss_avg /= len(loader)
+        # Promedios
+        c_loss_epoch /= len(loader)
+        g_loss_epoch /= len(loader)
+        w_dist_epoch /= len(loader)
 
-        # mostrar cada 20 épocas
-        if epoch % 20 == 0 or epoch == 1:
-            print(f"Epoch {epoch}/{epochs} | G_loss={g_loss_avg:.4f} | D_loss={d_loss_avg:.4f}")
+        print(f"Epoch {epoch}/{epochs} | "
+              f"W_dist={w_dist_epoch:.4f} | "
+              f"C_loss={c_loss_epoch:.4f} | "
+              f"G_loss={g_loss_epoch:.4f}")
 
-        # Guardar mejor modelo (según menor G_loss)
-        if g_loss_avg < best_g_loss - min_delta:
-            best_g_loss = g_loss_avg
+        if w_dist_epoch > best_w_dist + min_delta:
+            best_w_dist = w_dist_epoch
             epochs_no_improve = 0
-            torch.save(
-                {
-                    'G': G.state_dict(),
-                    'D': D.state_dict(),
-                    'epoch': epoch,
-                    'G_loss': g_loss_avg,
-                    'D_loss': d_loss_avg
-                },
-                os.path.join(save_dir, 'model_best.pth')
-            )
-            print(f"Nuevo mejor modelo en la época {epoch} | G_loss={g_loss_avg:.4f}")
+
+            torch.save({
+                'G': G.state_dict(),
+                'C': C.state_dict(),
+                'epoch': epoch,
+                'W_dist': best_w_dist
+            }, os.path.join(save_dir, 'best_model.pth'))
+
+            print("Nuevo mejor modelo guardado")
+
         else:
             epochs_no_improve += 1
 
         if epochs_no_improve >= patience:
-            print(f"Early stopping en época {epoch} (sin mejora en {patience} épocas)")
+            print(f"Early stopping en epoch {epoch}")
             break
 
     print("Entrenamiento completado. Modelos guardados en", save_dir)
-    print(f"Mejor G_loss: {best_g_loss:.4f}")
 
 if __name__ == "__main__":
     ### Parámetros de ejecución ###
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', type=str, default='data/dataset_nist.npy', help='Ruta del dataset .npy')
-    parser.add_argument('--epochs', type=int, default=100, help='Número de épocas de entrenamiento')
-    parser.add_argument('--batch_size', type=int, default=32, help='Tamaño del batch')
-    parser.add_argument('--z_dim', type=int, default=32, help='Dimensión del vector de ruido del generador')
-    parser.add_argument('--lr', type=float, default=2e-4, help='Learning rate (tasa de aprendizaje)')
-    parser.add_argument('--save_dir', type=str, default='checkpoints/WGAN_Conv1D',
-                        help='Carpeta donde guardar checkpoints')
+    parser.add_argument('--epochs', type=int, default=200, help='Número de épocas de entrenamiento')
+    parser.add_argument('--batch_size', type=int, default=64, help='Tamaño del batch')
+    parser.add_argument('--z_dim', type=int, default=64, help='Dimensión del vector de ruido del generador')
+    parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate (tasa de aprendizaje)')
+    parser.add_argument('--n_critic', type=int, default=5,help='Número de updates del critic por update del generator')
+    parser.add_argument('--lambda_gp', type=float,default=10.0, help='Peso del gradient penalty')
+    parser.add_argument('--patience', type=int, default=20,help='Paciencia para early stopping')
+    parser.add_argument('--min_delta', type=float,default=1e-4,help='Mejora mínima en Wasserstein distance')
+    parser.add_argument('--save_dir', type=str, default='checkpoints/WGAN_GP_Conv1D', help='Carpeta donde guardar checkpoints')
     parser.add_argument('--L', type=int, default=128, help='Longitud de las señales (número de muestras)')
-    parser.add_argument('--patience', type=int, default=10, help='Paciencia para early stopping')
-    parser.add_argument('--min_delta', type=float, default=1e-4, help='Mejora mínima en G_loss')
 
     args = parser.parse_args()
 
@@ -216,14 +203,16 @@ if __name__ == "__main__":
 
     # Crear instancias de ambos modelos
     G = Generator(args.z_dim, args.L).to(device)
-    D = Discriminator(args.L).to(device)
+    C = Discriminator(args.L).to(device)
     G.apply(weights_init)
-    D.apply(weights_init)
+    C.apply(weights_init)
 
-    train_gan(G,D, loader, device,
+    train_gan(G,C, loader, device,
               epochs = args.epochs,
               lr=args.lr,
-              save_dir=args.save_dir,
+              λ_gp=args.lambda_gp,
+              n_critic=args.n_critic,
+              z_dim=args.z_dim,
               patience=args.patience,
               min_delta=args.min_delta,
-              z_dim=args.z_dim)
+              save_dir=args.save_dir)
